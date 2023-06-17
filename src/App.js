5b7e80c5f9543af08cd3ab47192f8e594ad00799
Ashfaq147 <124809666+Ashfaq147@users.noreply.github.com>
2023-06-18 00:58:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './app.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedBrand, setSelectedBrand] = useState('');
+
+  const handleBrandClick = (brand) => {
+    setSelectedBrand(brand);
+  };
+
+  const getUsers = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get('https://reqres.in/api/users?page=1');
+      setUsers(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="navbar">
+        <div className="brand">Brand: {selectedBrand}</div>
+        <div className="brand-options">
+          <button
+            className={`btn ${selectedBrand === 'AirData' ? 'selected' : ''}`}
+            onClick={() => handleBrandClick('AirData')}
+          >
+            AirData
+          </button>
+          <button
+            className={`btn ${selectedBrand === 'Censusster' ? 'selected' : ''}`}
+            onClick={() => handleBrandClick('Censusster')}
+          >
+            Censusster
+          </button>
+          <button
+            className={`btn ${selectedBrand === 'Data Works' ? 'selected' : ''}`}
+            onClick={() => handleBrandClick('Data Works')}
+          >
+            Data Works
+          </button>
+        </div>
+        <button className="btn" onClick={getUsers}>Get users</button>
+      </nav>
+      {isLoading ? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <div className="grid-container">
+          {users.map((user) => (
+            <div className="user-card" key={user.id}>
+              <img className="avatar" src={user.avatar} alt={user.first_name} />
+              <p className="name">{`${user.first_name} ${user.last_name}`}</p>
+              <p className="email">{user.email}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
